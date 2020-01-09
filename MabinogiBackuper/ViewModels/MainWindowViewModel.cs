a50cb27b74389bab91ae7ec2fc95b44235cb38d5
AonaSuzutsuki <rvvfake.org@gmail.com>
2020-01-10 00:58:08 +0900

    added navigationbase

---
+++ MabinogiBackuper/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+using CommonStyleLib.ViewModels;
+using CommonStyleLib.Views;
+using MabinogiBackuper.Models;
+using MabinogiBackuper.Views;
+using MabinogiBackuper.Views.Pages;
+using Prism.Commands;
+
+namespace MabinogiBackuper.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public MainWindowViewModel(WindowService service, MainWindowModel model) : base(service, model)
+        {
+            this.model = model;
+
+            BackupBtCommand = new DelegateCommand(OpenBackup);
+        }
+
+        #region Fields
+
+        private MainWindowModel model;
+
+        #endregion
+
+        #region Properties
+
+
+
+        #endregion
+
+        #region Event Properties
+
+        public ICommand BackupBtCommand { get; set; }
+
+        #endregion
+
+        #region Event Methods
+
+        public void OpenBackup()
+        {
+            var navigationModel = new NavigationBaseModel();
+            WindowManageService.Show<NavigationBase>(window =>
+            {
+                var service = new NavigationWindowService
+                {
+                    Owner = window,
+                    Navigation = window.MainFrame.NavigationService,
+                };
+                service.Pages = new List<Page>
+                {
+                    new TestPage(service)
+                };
+                var vm = new NavigationBaseViewModel(service, navigationModel);
+                window.Loaded += (sender, args) => vm.Loaded.Execute(null);
+                return vm;
+            });
+        }
+
+        #endregion
+    }
+}
---
+++ MabinogiBackuper/ViewModels/NavigationBaseViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using CommonStyleLib.ViewModels;
+using CommonStyleLib.Views;
+using MabinogiBackuper.Models;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+
+namespace MabinogiBackuper.ViewModels
+{
+    public class NavigationWindowService : WindowService
+    {
+        public class NavigationBindableValue : BindableBase
+        {
+            private bool _canGoNext;
+            private bool _canGoBack;
+
+            private Visibility _closeBtVisibility;
+
+            public bool CanGoNext
+            {
+                get => _canGoNext;
+                set => SetProperty(ref _canGoNext, value);
+            }
+
+            public bool CanGoBack
+            {
+                get => _canGoBack;
+                set => SetProperty(ref _canGoBack, value);
+            }
+
+            public Visibility CloseBtVisibility
+            {
+                get => _closeBtVisibility;
+                set => SetProperty(ref _closeBtVisibility, value);
+            }
+        }
+
+        public IList<Page> Pages { get; set; }
+        public NavigationService Navigation { get; set; }
+
+        public NavigationBindableValue NavigationValue { get; } = new NavigationBindableValue();
+
+        public void GoNext()
+        {
+
+        }
+
+        public void GoBack()
+        {
+
+        }
+    }
+
+    public class NavigationBaseViewModel : ViewModelBase
+    {
+        public NavigationBaseViewModel(NavigationWindowService service, NavigationBaseModel model) : base(service, model)
+        {
+            _navigationService = service;
+
+            CloseBtVisibility = service.NavigationValue.ObserveProperty(m => m.CloseBtVisibility).ToReactiveProperty();
+        }
+
+        #region Fields
+
+        private readonly NavigationWindowService _navigationService;
+
+        #endregion
+
+        #region Properties
+
+        public ReactiveProperty<Visibility> CloseBtVisibility { get; set; }
+
+        #endregion
+
+
+        #region Event Methods
+
+        protected override void MainWindow_Loaded()
+        {
+            _navigationService.Navigation.Navigate(_navigationService.Pages.First());
+        }
+
+        #endregion
+    }
+}
