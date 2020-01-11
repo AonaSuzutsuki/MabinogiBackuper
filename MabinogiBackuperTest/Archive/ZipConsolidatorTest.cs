43708575b3777bfef829d0d9ac154031320ca064
AonaSuzutsuki <rvvfake.org@gmail.com>
2020-01-12 03:40:37 +0900

    added test proj.

---
+++ MabinogiBackuperTest/Archive/ZipConsolidatorTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonCoreLib.CommonPath;
+using MabinogiBackuperLib.Archive;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace MabinogiBackuperTest.Archive
+{
+    [TestFixture]
+    public class ZipConsolidatorTest
+    {
+        [Test]
+        public void CreateEntryNameTest()
+        {
+            var di = new DirectoryInfo($"{TestContext.CurrentContext.TestDirectory}/TestData".UnifiedSystemPathSeparator());
+            var files = di.GetFiles("*", SearchOption.AllDirectories).Select(x => x.FullName);
+
+            foreach (var file in files)
+            {
+                ZipConsolidator.CreateEntryName(file, di.FullName);
+            }
+        }
+    }
+}
---
+++ MabinogiBackuperTest/Backup/RegistryCollectorTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MabinogiBackuperLib.Backup;
+using MabinogiBackuperLib.ExRegistry;
+using Microsoft.Win32;
+using Moq;
+using NUnit.Framework;
+
+namespace MabinogiBackuperTest.Backup
+{
+    [TestFixture]
+    public class RegistryCollectorTest
+    {
+        [Test]
+        public void GetJsonTest()
+        {
+            var registryPath = @"Software\Nexon\Mabinogi";
+            var exp = "{\"ThemePack\":\"ネオンピンク\",\"ThemePackColorSet\":\"半透明\",\"TpsMode\":\"0\"}";
+
+            var moq = new Mock<IRegistryEditor>();
+            moq.Setup(x => x.GetKeyNames(registryPath)).Returns(new []
+            {
+                "ThemePack", "ThemePackColorSet", "TpsMode"
+            });
+            moq.Setup(x => x.GetValue(registryPath, "ThemePack", Registry.CurrentUser)).Returns("ネオンピンク");
+            moq.Setup(x => x.GetValue(registryPath, "ThemePackColorSet", Registry.CurrentUser)).Returns("半透明");
+            moq.Setup(x => x.GetValue(registryPath, "TpsMode", Registry.CurrentUser)).Returns("0");
+            var regEdit = moq.Object;
+
+            var collector = new RegistryCollector
+            {
+                RegistryEditor = regEdit
+            };
+            var json = collector.GetJson();
+
+            Assert.AreEqual(exp, json);
+        }
+    }
+}
